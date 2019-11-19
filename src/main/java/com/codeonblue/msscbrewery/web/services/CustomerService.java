26008b60073276c8f79881bc94742780086099bc
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2019-11-19 18:55:05 -0300

    Add Customer endpoints for GET, POST, PUT and DELETE

--- src/main/java/com/codeonblue/msscbrewery/web/services/CustomerService.java
+++ src/main/java/com/codeonblue/msscbrewery/web/services/CustomerService.java
@@ -5,5 +5,13 @@ import com.codeonblue.msscbrewery.web.model.CustomerDto;
 import java.util.UUID;
 
 public interface CustomerService {
+
     CustomerDto getCustomerById(UUID customerId);
+
+    CustomerDto saveNewCustomer(CustomerDto customerDto);
+
+    void updateCustomer(UUID customerId, CustomerDto customerDto);
+
+    void deleteById(UUID customerId);
+
 }
