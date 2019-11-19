4b3cf407d0048c57796283b098bfb1a9b9df5346
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2019-11-19 18:35:42 -0300

    Add delete endpoint

--- src/main/java/com/codeonblue/msscbrewery/web/services/BeerServiceImpl.java
+++ src/main/java/com/codeonblue/msscbrewery/web/services/BeerServiceImpl.java
@@ -1,10 +1,12 @@
 package com.codeonblue.msscbrewery.web.services;
 
 import com.codeonblue.msscbrewery.web.model.BeerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class BeerServiceImpl implements BeerService {
 
@@ -26,5 +28,10 @@ public class BeerServiceImpl implements BeerService {
     @Override
     public void updateBeer(UUID beerId, BeerDto beerDto) {
 
+    }
+
+    @Override
+    public void deleteById(UUID beerId) {
+        log.debug("Deleting a beer...");
     }
 }
