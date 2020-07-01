e69273f9282e62613ea436ef35a7fb8d94c161dc
James Daniel Correia de Freitas <jamesfrj@yahoo.com.br>
2020-07-01 07:55:17 -0300

    Date conversion with MapStruct

--- src/main/java/com/codeonblue/msscbrewery/web/mappers/BeerMapper.java
+++ src/main/java/com/codeonblue/msscbrewery/web/mappers/BeerMapper.java
@@ -4,7 +4,7 @@ import com.codeonblue.msscbrewery.domain.Beer;
 import com.codeonblue.msscbrewery.web.model.BeerDto;
 import org.mapstruct.Mapper;
 
-@Mapper
+@Mapper(uses = {DateMapper.class})
 public interface BeerMapper {
 
     BeerDto beerToBeerDto(Beer beer);
